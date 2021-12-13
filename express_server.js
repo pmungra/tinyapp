6f0e5c7c93e30ba47c353e6c3f97a1e5c82c122d
Your Name <parth.mungra94@gmail.com>
2021-12-13 13:53:19 +0000

    add logic and clean code

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(cookie());
 app.use(morgan('dev'));
 
-const {verifyShortUrl, randomString, checkIfAvail, addUser, fetchUserInfo} = require('./helper');
+const {verifyShortUrl, randomString, checkIfAvail, addUser, fetchUserInfo, currentUser, urlsForUser} = require('./helper');
 
 //Redirect Short URLs
 const urlDatabase = {
@@ -36,68 +36,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
 // Adding Route for /urls  to displayed on the main page
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase, current_user: currentUser(req.cookies['user_id']) };
   res.render("urls_index", templateVars);
 //  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-// Adding GET Route to Show the Form (new url is created)
-//Adding an endpoint to handle a POST to /login in your Express server
-app.get("/urls/new", (req, res) => {
-  const current_user = currentUser(req.cookies['user_id'])
-  if (!current_user) {
-    res.redirect('/login');
-  }
-  let templateVars = { current_user: current_user }
-  res.render("urls_new", templateVars);
-});
-
-// Redirecting to new Page
-app.get("/urls/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  if (verifyShortUrl(shortURL, urlDatabase)) {
-    let longURL = urlDatabase[req.params.shortURL];
-    let templateVars = { shortURL: shortURL, longURL: longURL, current_user: currentUser(req.cookies['user_id'])};
-    res.render("urls_show", templateVars);
-  } else {
-    res.send('does not exist')
-  }});
-
-// Adding POST Route to Receive the Form Submission
-app.post("/urls", (req, res) => {
-  const shortURL = randomString();
-  const newURL = req.body.longURL;
-  urlDatabase[shortURL] = newURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
-
-// Redirecting to longURL
-app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  if (verifyShortUrl(shortURL, urlDatabase)) {
-    const longURL = urlDatabase[shortURL];
-    res.redirect(longURL);
-  } else {
-    res.status(404);
-    res.send('Page does not exist');
-  }
-});
-
-//Delete: Routing on Server ==>Add a POST route that removes a URL resource: POST /urls/:shortURL/delete ===>redirect the client back to the urls_index page ("/urls")
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const urlToDelete = req.params.shortURL;
-  delete urlDatabase[urlToDelete];
-  res.redirect('/urls');
-});
-
-//Edit: Add a POST route that updates a URL resource; POST /urls/edit
-app.post("/urls/:shortURL/edit", (req, res) => {
-  const edit = req.params.shortURL;
-  urlDatabase[edit] = req.body.longURL;
-  res.redirect(`/urls`)
 });
 
 // Registration form
@@ -143,6 +88,91 @@ app.post("/login", (req, res) => {
     res.status(403).send('Error 403... email not found')
   }
 });
+
+//urls are displayed on the main page
+app.get("/urls", (req, res) => {
+  const current_user = currentUser(req.cookies['user.id'], userDatabase);
+  if (!current_user) {
+    res.send("<html><body>Please sign in or register</body></html");
+  }
+  const userLinks = urlsForUser(current_user, urlDatabase);
+  let templateVars = { urls: urlDatabase, current_user: currentUser(req.cookies['user_id'], userDatabase) };
+  res.render("urls_index", templateVars);
+});
+
+// Adding POST Route to Receive the Form Submission
+//To add new url to all urls page
+app.post("/urls", (req, res) => {
+  const shortURL = randomString();
+  const newURL = req.body.longURL;
+  const user = currentUser(req.cookie['user_id'], userDatabase);
+  urlDatabase[shortURL] = { longURL: newURL, userID: user};
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// new url key is created
+app.get("/urls/new", (req, res) => {
+  const current_user = currentUser(req.cookies['user_id'], userDatabase);
+  if (!current_user) {
+    res.redirect('/login');
+  }
+  let templateVars = { current_user: current_user }
+  res.render("urls_new", templateVars);
+});
+
+// Redirecting to new Page
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const current_user = currentUser(req.cookies['user_id'], userDatabase);
+  if (!urlDatabase[shortURL]) {
+    res.send("The link does not exist");
+  } else if (current_user !== urlDatabase[shortURL].userID) {
+    res.send('This id does not belong to you');
+    }
+  if (verifyShortUrl(shortURL, urlDatabase)) {
+    let longURL = urlDatabase[req.params.shortURL].longURL;
+    let templateVars = { shortURL: shortURL, longURL: longURL, current_user: currentUser(req.cookies['user_id'], userDatabase)};
+    res.render("urls_show", templateVars);
+  } else {
+    res.send('does not exist');
+  }
+});
+
+// Redirecting to longURL
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  if (verifyShortUrl(shortURL, urlDatabase)) {
+    const longURL = urlDatabase[shortURL].longURL;
+    res.redirect(longURL);
+  } else {
+    res.status(404);
+    res.send('Page does not exist');
+  }
+});
+
+//Delete: Routing on Server ==>Add a POST route that removes a URL resource: POST /urls/:shortURL/delete ===>redirect the client back to the urls_index page ("/urls")
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const current_user = currentUser(req.cookies['user_id'], userDatabase);
+  const shortURL = req.params.shortURL;
+  if (current_user !== urlDatabase[shortURL].userID) {
+    res.send('This id does not belong to you');
+  }
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
+//Edit: Add a POST route that updates a URL resource; POST /urls/edit
+app.post("/urls/:shortURL/edit", (req, res) => {
+  if (!checkOwner(currentUser(req.cookies['user_id'], userDatabase), req.params.shortURL, urlDatabase)) {
+    res.send('This id does not belong to you')
+  }
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+  res.redirect('/urls')
+});
+const checkOwner = (userId, urlID, database) => {
+  console.log('this is shortURL', urlID)
+  return userId === database[urlID].userID
+};
 
 // endpoint user logout
 app.post("/logout", (req, res) => {
